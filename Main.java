0e7ff5a2c0daf5c32cd781414ff159d62739f3be
arun <arunsardana02@gmail.com>
2025-05-24 21:33:46 +0530

    2nd commit

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ import java.util.concurrent.Executors;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO	gi Auto-generated method stub
 	
 		ExecutorService es = Executors.newFixedThreadPool(5);
 		for(int i=1;i<100;i++) {
